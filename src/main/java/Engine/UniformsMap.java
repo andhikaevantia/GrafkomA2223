ecc8bb9c5e4bbdf030d392ab3d5e0887aa23c399
jovanliu777 <131933883+jovanliu777@users.noreply.github.com>
2023-05-29 10:01:58 +0700

    Uniforms Map Without Create Uniform Function

--- src/main/java/Engine/UniformsMap.java
+++ src/main/java/Engine/UniformsMap.java
@@ -3,27 +3,30 @@ package Engine;
 import org.joml.*;
 import org.lwjgl.system.MemoryStack;
 
+import java.nio.IntBuffer;
 import java.util.*;
 
 import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
 
 public class UniformsMap {
-
-    private int programId;
-    private Map<String, Integer> uniforms;
+    private final Map<String, Integer> uniforms;
 
     public UniformsMap(int programId) {
-        this.programId = programId;
         uniforms = new HashMap<>();
-    }
 
-    public void createUniform(String uniformName) {
-        int uniformLocation = glGetUniformLocation(programId, uniformName);
-        if (uniformLocation < 0) {
-            throw new RuntimeException("Could not find uniform [" + uniformName + "] in shader program [" +
-                    programId + "]");
+        int uniformCount = glGetProgrami(programId, GL_ACTIVE_UNIFORMS);
+
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer tempBuffer = stack.mallocInt(1);
+            for (int i = 0; i < uniformCount; i++) {
+                String key = glGetActiveUniform(programId, i, glGetProgrami(programId, GL_ACTIVE_UNIFORM_MAX_LENGTH), tempBuffer, tempBuffer);
+
+                int location = glGetUniformLocation(programId, key);
+
+                uniforms.put(key, location);
+            }
         }
-        uniforms.put(uniformName, uniformLocation);
     }
 
     private int getUniformLocation(String uniformName) {
@@ -31,7 +34,7 @@ public class UniformsMap {
         if (location == null) {
             throw new RuntimeException("Could not find uniform [" + uniformName + "]");
         }
-        return location.intValue();
+        return location;
     }
 
     public void setUniform(String uniformName, int value) {
@@ -50,5 +53,8 @@ public class UniformsMap {
 
     public void setUniform(String uniformName, Vector3f value) {
         glUniform3f(getUniformLocation(uniformName), value.x, value.y, value.z);
+    }
+    public void setUniform(String uniformName, Float value) {
+        glUniform1f(getUniformLocation(uniformName), value);
     }
 }
